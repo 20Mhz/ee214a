ab02f82986f4f438d8b60a3574cb829b45271a95
Ronald Valenzuela <ronaldv@myth11.Stanford.EDU>
2013-11-06 17:47:41 -0800

    Initial Check-In

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ee214a
 ======
 
 EE214A Course Project
+
+Students:
+	Hari Vemuri
+	Ronald Valenzuela
---
+++ scripts/opt.m
@@ -0,0 +1,23 @@
+% EE2124 Matlab Script to Optimize Transimpedance Amplifier
+clear all;
+addpath('/usr/class/ee114/matlab/hspice_toolbox')
+
+% Parameters
+
+% Models 
+% ZVTC = ;
+
+%% Load Simulation Data 
+%h = loadsig('part3.sw0');
+%lssig(h)
+%idn = evalsig(h,'i_mn1');
+
+% Visualization
+%figure(1);
+%plot(vds, idn, 'linewidth', 2);
+%set(gca,'FontSize',14);
+%title('NMOS 10/1')
+%xlabel('V_D_S [V]');
+%ylabel('I_D [A]');
+%grid;
+
