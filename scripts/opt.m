7ff4b990b6f31c5641c9d6d76e3bc45e6e8319c3
Ronald Valenzuela <ronaldv@Ronalds-MacBook-Pro.local>
2013-11-10 13:16:01 -0300

    update

--- scripts/opt.m
+++ scripts/opt.m
@@ -7,9 +7,9 @@ format short eng;
 
 % Parameters
 uCox = 50e-6;
-Cl = 500e-15/2;
-Rl = 10e3;
-Vtn = 0.5;
+Cl = 250e-15;
+Rl = 10e3/2;
+Vtn = 1; % Closer than Vth0
 Lmin=1e-6;
 lambda=0.1;
 VDD= 2.5;
@@ -18,41 +18,64 @@ VDD= 2.5;
 freq_target = 100e6; % > 90
 power_target = 2e-3; % 5v * Itotal
 I_total = 2e-3/5; % 400uA
-
+Rspec=25e3;
 % Initial Estimations
 VOV3=0.3;
 
 %% gm3
 tau_target=1/(freq_target*2*pi); % Total available Tau
-tau_target_out=0.1*tau_target; % Guess tau out = 10% of tau Total
-gm3 = Cl/tau_target_out;
-Id3 = VOV3*gm3/2; % seems too high
+tau_target_out=0.15*tau_target; % Guess tau out = 10% of tau Total
+gm3 = (Cl/tau_target_out);
+Id3 = VOV3*gm3/2 % 
 
 % Set Id2 & 3 based on budget
-Id2= (I_total-2*Id3)/4;% too high
-Id1= (I_total-2*Id3)/4;%
-Id2= 40e-6;
-Id1= 40e-6;
+Id2= (I_total-2*Id3)/4*4/3% 
+Id1= (I_total-2*Id3)/4*2/3%
 
 %% gm_l2
 Vg3 = VOV3 + 0 + Vtn; 
 VOV_L2 = (VDD - Vg3 - Vtn);
-gm_l2 = 2*Id2/VOV_L2;
-R_l2 = 1/gm_l2
+gm_l2 = (2*Id2/VOV_L2)/1.2;
+R_l2 = 1/gm_l2;
 
+%% gm_l1 
+VOV_L1 = sqrt(2*Id1/(uCox*2))
+gm_l1 = 2*Id1/VOV_L1;
+R_l1 = 1/gm_l1;
+
+%% gm2
+gm2 = 1.5*Rspec/(R_l1*R_l2);
+VOV2= 2*Id2/gm2;
+
+%% gm1
+VOV1BIAS=1;
+VOV1 = 0 - (VOV1BIAS-2.5)- Vtn;
+gm1 = 2* Id1/VOV1;
 
 % Core Transistor Sizing
-M3 = 2*Id3/(uCox*VOV3^2)*Lmin
-M3_ratio = round(M3/Lmin)
-M_L2 = gm_l2/(uCox*VOV_L2)*Lmin
-M_L2_ratio = round(M_L2/Lmin)
+%W3 = 2*Id3/(uCox*VOV3^2*(1+lambda*2.5))*Lmin;
+W3 = *Lmin;
+W3_ratio = max(2,round(W3/Lmin));
+
+W_L2 = gm_l2/(uCox*VOV_L2)*Lmin;
+W_L2_ratio = max(2,round(W_L2/Lmin));
+W2 = gm2/(uCox*VOV2)*Lmin;
+W2_ratio = max(2,round(W2/Lmin));
+
+W_L1 = gm_l1/(uCox*VOV_L1)*Lmin;
+W_L1_ratio = max(2,round(W_L1/Lmin));
+
+W1 = gm1/(uCox*VOV1)*Lmin;
+W1_ratio = max(2,round(W1/Lmin));
+
+sprintf('.param W_1=%du\n.param W_2=%du\n.param W_3=%du\n.param W_L1=%du\n.param W_L2=%du',W1_ratio,W2_ratio,W3_ratio,W_L1_ratio,W_L2_ratio)
+return
 
 % Bias source sizing based on current requirements
 
 % Craete spice file 
 % fwrite(fid, evalc('disp(M3)'))
 
-return
 %% Models 
 % Gain
 Reff = Rcg * gm2 * Rd2;
